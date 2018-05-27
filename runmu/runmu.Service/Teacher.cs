d6ef7334779c48d97f8f9f56eb8b4341430a4b90
rfqu <289198777@qq.com>
2018-05-28 00:11:08 +0800

    init project

---
+++ runmu/runmu.Service/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace runmu.Service
+{
+    public class Teacher
+    {
+        string name;
+        int id;
+        string qq;
+        string phone;
+
+        public string Name { get => name; set => name = value; }
+        public int Id { get => id; set => id = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public string Qq { get => qq; set => qq = value; }
+    }
+}
